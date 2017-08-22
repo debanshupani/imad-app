5b9d0d5bbd6b7b97f589db8726e430ff80bedc61
debanshupani <31133634+debanshupani@users.noreply.github.com>
2017-08-22 23:55:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,11 +35,12 @@ var articleOne = {
 };
 
 function createTemplate (data) {
-    var title = data.title
-    var date = data.date
-    var heading = data.heading 
-    var content = data.content
-    var htmlTemplate 
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading; 
+    var content = data.content;
+    
+    var htmlTemplate = `
     <html>
         <head>
             <title>
